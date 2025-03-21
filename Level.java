dfa3569861f5ba695c8fc1a8cc1ff36a11222065
HaoXK <haoxuank@nycstudents.net>
2025-03-21 12:25:02 -0400

    Update Level.java

--- Level.java
+++ Level.java
@@ -1,1 +1,19 @@
+public class Level {
+    private boolean goalReached;
+    private int points;
+    public boolean goalReached() {
+        return goalReached;
+    }
 
+    public void reachGoal() {
+        goalReached = true;
+    }
+    public int getPoints() {
+        return points;
+    }
+    
+    public void setPoints(int p) {
+        points = p;
+    }
+}
+                            
